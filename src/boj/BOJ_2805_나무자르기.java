2cb41c598be4c9ed0348a71cf8dc0261d350de78
hyk339 <hyk339@gmail.com>
2022-03-06 20:41:51 +0900

    BOJ 2805 나무자르기 solved

--- src/boj/BOJ_2805_나무자르기.java
+++ src/boj/BOJ_2805_나무자르기.java
@@ -3,42 +3,47 @@ package boj;
 import java.io.*;
 import java.util.*;
 
+// 2022.03.05 unsolved
+// 2022.03.06 solved 
+//  1. 자르고 난 나무들의 길이의 합이 내가 필요한 길이보다 크거나 같으면 left를 키워야한다.
+//  2. 문제를 잘 읽자. 문제에 주어지는 나무의 최대의 길이가 20억으로 되어있다. 따라서 데이터 타입을 long으로 주어야한다.
+
 public class BOJ_2805_나무자르기 {
 	
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
-	static int[] arr;
-	static int max = 0;
+	static long[] arr;
+	static long max = 0;
 	public static void main(String[] args) throws IOException {
+		
 		String firstRow = br.readLine();
 		st = new StringTokenizer(firstRow," ");
 		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+		long M = Integer.parseInt(st.nextToken());
 	
-		arr = new int[N];
+		arr = new long[N];
 		st = new StringTokenizer(br.readLine()," ");
 		for(int i=0; i<N; i++) {
 			arr[i] = Integer.parseInt(st.nextToken());
 			if(arr[i]>max) max = arr[i];
 		}
 		
-		int left = 0;
-		int right = max;
-		while(left<right) {
-			int length = 0;
-			int mid = (left+right)/2;
+		long left = 0;
+		long right = max;
+		while(left<=right) {
+			long length = 0;
+			long mid = (left+right)/2;
 			for(int j=0; j<N; j++) {
 				if(mid<arr[j]) {
 					length += arr[j]-mid;
 				}
 			}
 			if(length >= M) {
-				right = mid-1;
-			}else {
 				left = mid +1;
+			}else {
+				right = mid-1;
 			}
 		}
 		System.out.println(right);
-		
 	}
 }
