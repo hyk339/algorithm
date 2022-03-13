21564892f20bb77a44dbc8876ee7647ae87ca786
hyk339 <hyk339@gmail.com>
2022-03-13 21:06:34 +0900

    BOJ 2630 색종이만들기 Test

--- src/boj/BOJ_2630_색종이만들기.java
+++ src/boj/BOJ_2630_색종이만들기.java
@@ -9,6 +9,9 @@ public class BOJ_2630_색종이만들기 {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	static int[][] arr;
+	static int blueCount = 0;
+	static int whiteCount = 0;
+	
 	
 	public static void main(String[] args) throws IOException {
 		int N = Integer.parseInt(br.readLine());
@@ -22,21 +25,37 @@ public class BOJ_2630_색종이만들기 {
 			}
 		}
 		
-		func(N);
+		func(0,0,N); //색종이만들기 함수시작
+		System.out.println(whiteCount);
+		System.out.println(blueCount);
+		
 	}
 	
-	public static void func(int m) {
-		int first = 0;
-		for(int y=0; y<m; y++) {
-			for(int x=0; x<m; x++) {
-				if(y==0 && x==0) {first = arr[y][x];}
-				if(first != arr[y][x]) {
-					//func(m/2);
-					
-				
-				}
+	public static void func(int row, int column, int m) {
+		int first = arr[row][column]; //가장 첫번째 나오는 색종이색 
+		System.out.println("row : "+row+", column : "+column);
+		System.out.println("first : "+first);
+		
+		for(int y=row; y<row+m; y++) {
+			for(int x=column; x<column+m; x++) {
+				System.out.println("arr["+y+"]["+x+"] : "+ arr[y][x]);
+				if(arr[y][x] != first) { //가장 처음 나온 종이의 색과 같이 않을 경우
+					func(row, column, m/2);
+					func(row, column+m/2, m/2);
+					func(row+m/2, column, m/2);
+					func(row+m/2, column+m/2, m/2);
+					return;
+				}		
 			}
 		}
 		
+		if(first == 0) { //하얀색이면
+			whiteCount++;
+			System.out.println("whiteCount : "+whiteCount);
+		}else if(first == 1){ //파란색이면
+			blueCount++;
+			System.out.println("blueCount : "+blueCount);
+		}
 	}
+	
 }
