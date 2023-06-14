43db60d540d6008e7c65380508ec5765a5f2fcc7
hyk <hyk339@gmail.com>
2023-06-15 00:57:41 +0900

    BOJ 1629 곱셈 solved

--- src/boj/BOJ_1629_곱셈.java
+++ src/boj/BOJ_1629_곱셈.java
@@ -5,11 +5,9 @@ import java.util.*;
 
 public class BOJ_1629_곱셈 {
 	
-	static int A;
-	static int B;
-	static int C;
-	static String[] pattern;
-	static int result;
+	static long A;
+	static long B;
+	static long C;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,53 +15,21 @@ public class BOJ_1629_곱셈 {
 		A = Integer.parseInt(st.nextToken());
 		B = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
-		pattern = new String[C];
 		
-		Arrays.fill(pattern,"N");
-		
-		findRemain();
-		
+		long result = func(A,B,C);
+		System.out.println(result);
+
 	}
-	
-	public static void findRemain() {
-		
-		boolean check = false;
-		int number = 1;
-		int curr = A;
-		int beforeDivnumber = 1;
-		if(curr>=C) pattern[curr%C] = number+"";
-		
-		
-		while(number<=B) { //거듭제곱 
 
-			curr = curr*A;
-			++number;
-			
-			if(curr>=C) {
-				if(pattern[curr%C].equals("N")) {
-					pattern[curr%C] = number+ "";
-				}else {
-					check = true; //패턴을 찾았다면 true로 바꾼다.
-					break;
-				}
-			}else {
-				beforeDivnumber = number;
-			}
+	public static long func(long a, long b, long c) {
+		if(b==0) {
+			return 1;
 		}
-		
-		//거듭제곱을 해도 나누는 값보다 작을때
-		if(curr < C) { 
-			result = curr;
-
-		}else if(!check) { //패턴을 찾기 전에 거듭제곱 횟수가 적어 끝났을경우
-			for(int i=0; i<C; i++) {
-				if(pattern[i].equals(String.valueOf(number))) {
-					result = i;
-					return;
-				}
-			}
-		}else { //패턴을 찾아야하는 경우
-			
+		if(b%2 == 1) {
+			return (a * (func(a,b-1,c))) % c;
 		}
+		long k = func(a,b/2,c);
+		return (k*k)%c;
 	}
 }
+
