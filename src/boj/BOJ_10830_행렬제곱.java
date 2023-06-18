bf5b4165c0f82bb5d1baaf16f60808d192312278
hyk <hyk339@gmail.com>
2023-06-18 12:15:39 +0900

    BOJ 10830 행렬제곱 solved

--- src/boj/BOJ_10830_행렬제곱.java
+++ src/boj/BOJ_10830_행렬제곱.java
@@ -10,12 +10,14 @@ public class BOJ_10830_행렬제곱 {
 	static final int MOD = 1000; 
 	static int[][] originArr;
 	static StringTokenizer st;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
-		B = Integer.parseInt(st.nextToken());
+		// 2023.06.18 parse Long을 해야한다.
+		B = Long.parseLong(st.nextToken());
 		
 		originArr = new int[N][N];
 		//행
@@ -23,7 +25,7 @@ public class BOJ_10830_행렬제곱 {
 			st = new StringTokenizer(br.readLine());
 			//열
 			for(int j=0; j<N; j++) { 
-				originArr[i][j] = Integer.parseInt(st.nextToken());
+				originArr[i][j] = Integer.parseInt(st.nextToken()) % MOD;
 			}
 		}
 		
@@ -44,12 +46,16 @@ public class BOJ_10830_행렬제곱 {
 			return arr1;
 		}
 		
-		if(num%2 == 1L) {
-			return multiply(arr1,func(arr1,num-1));
-		}
 		
 		int[][] temp = func(arr1,num/2);
-		return multiply(temp,temp);
+
+		temp = multiply(temp,temp); 
+		
+		if(num%2 == 1L) {
+			temp = multiply(originArr,temp);
+		}
+		
+		return temp;
 	}
 	
 	//행렬의 곱과 나머지를 계산하는 메서드
