bd10fddfffa121162eef1605d8dfe3b882ce10fe
hyk <hyk339@gmail.com>
2023-06-20 21:57:43 +0900

    BOJ 9461 파도반수열 solved

--- src/boj/BOJ_9461_파도반수열.java
+++ src/boj/BOJ_9461_파도반수열.java
@@ -5,25 +5,32 @@ import java.util.*;
 
 public class BOJ_9461_파도반수열 {
 	public static int t;
+	public static Long[] arr = new Long[101];
 	public static StringBuilder sb = new StringBuilder(); 
 	
 	public static void main(String[] args) throws IOException {
+		
+		arr[1] = 1L;
+		arr[2] = 1L;
+		arr[3] = 1L;
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		t = Integer.parseInt(br.readLine());
 		
 		for(int i=0; i<t; i++) {
 			int temp = Integer.parseInt(br.readLine());
-			sb.append(func(1,1,1,1,temp)).append("\n");
+			sb.append(func(temp)).append("\n");
 		}
 		System.out.println(sb);
 	}
 	
-	public static int func(int num, int val1, int val2, int val3, int purpose) {
-		if(num == purpose) {
-			return val1;
+	public static Long func(int N) {
+		if(arr[N] == null) {
+			arr[N] = func(N-3)+func(N-2);
 		}
-		return func(num+1,val2,val3,val1+val2,purpose);
+		
+		return arr[N];
 	}
 	
 }
