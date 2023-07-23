4d67ce464d2c827d2a312eefa7ff78edf9569ce9
hyk <hyk339@gmail.com>
2023-07-23 22:12:36 +0900

    BOJ 10844 쉬운계단수 230723 solved

--- src/boj/BOJ_10844_쉬운계단수_230723.java
+++ src/boj/BOJ_10844_쉬운계단수_230723.java
@@ -7,34 +7,40 @@ public class BOJ_10844_쉬운계단수_230723 {
 	
 	/*
 	 * 20230723 시간초과
+	 * 20230723 결국 전에 풀었던 풀이를 보았다.
+	 *          각자리수 까지의 횟수를 저장할때 1000000000 이상을 저장할 필요없이
+	 *          나눈 나머지 값만 저장하면 된다.
 	 */
 	static int N;
+	static Long[][] dp;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
+		dp = new Long[N+1][10];
 		long result = 0;
 		for(int i=1; i<=9; i++) {
-			result += fun(i,1) % 1000000000;			
+			result += fun(1,i);			
 		}
 		result %= 1000000000;
 		System.out.println(result);
 	}
 	
-	static long fun(int num, int idx) {
+	static long fun(int idx, int num) {
 		
-		long temp = 1L;
-		if(idx == N) {
-			return temp;
-		}else {
-			if(num == 0) {
-				temp =  fun(1,idx+1);
-			}else if(num == 9) {
-				temp =  fun(8,idx+1);
+		if(dp[idx][num] == null) {
+			if(idx == N) {
+				dp[idx][num] = 1L;
 			}else {
-				temp = fun(num-1,idx+1)+fun(num+1,idx+1);
+				if(num == 0) {
+					dp[idx][num] =  fun(idx+1,1);
+				}else if(num == 9) {
+					dp[idx][num] =  fun(idx+1,8);
+				}else {
+					dp[idx][num] = fun(idx+1, num-1)+fun(idx+1, num+1);
+				}
 			}
 		}
-		return temp;
+		return dp[idx][num] % 1000000000;
 	}
 }
 
