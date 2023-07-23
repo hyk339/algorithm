6659a60ae3403dcc48c6074033da26e5417ddd33
hyk <hyk339@gmail.com>
2023-07-23 14:39:02 +0900

    BOJ 1463 1로만들기 230723 solved

--- src/boj/BOJ_1463_1로만들기_230723.java
+++ src/boj/BOJ_1463_1로만들기_230723.java
@@ -4,31 +4,39 @@ import java.io.*;
 import java.util.*;
 
 public class BOJ_1463_1로만들기_230723 {
+	
+	
 	/*
 	 * 2023.07.23 시간초과..
+	 * 2023.07.23 최소의 횟수를 구한 것을 따로 저장해주지 않으면 
+	 * 		      다시 최소 횟수를 찾기위해 다시 계산해야한다.
+	 *            그래서 시간초과가 발생한다. 
 	 */
+	
+	static Integer[] dp;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int num = Integer.parseInt(br.readLine());
+		dp = new Integer[num+1];
+		dp[1] = 0;
 		int result = fun(num);
 		System.out.println(result);
 	}
+	
 	static int fun(int n) {
 		
-		int result = 0;
-		if(n == 1) {
-			return result;
-		}else {
+		if(dp[n] == null){
 			if(n%6 == 0) {
-				result = Math.min(Math.min(fun(n/3), fun(n/2)),fun(n-1))+1;
+				dp[n] = Math.min(Math.min(fun(n/3), fun(n/2)),fun(n-1))+1;
 			}else if(n%3 == 0) {
-				result = Math.min(fun(n/3), fun(n-1))+1;				
+				dp[n] = Math.min(fun(n/3), fun(n-1))+1;				
 			}else if(n%2 == 0) {
-				result = Math.min(fun(n/2), fun(n-1))+1;				
+				dp[n] = Math.min(fun(n/2), fun(n-1))+1;				
 			}else {
-				result = fun(n-1)+1;
+				dp[n] = fun(n-1)+1;
 			}
 		}
-		return result;	
+		return dp[n];	
 	}
 }
