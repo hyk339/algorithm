c8d151ed05ff8ebc95ee8eacc7a6169700e579d8
hyk <hyk339@gmail.com>
2024-03-21 22:30:49 +0900

    BOJ 2869 달팽이는올라가고싶다 solved

--- src/boj/BOJ_2869_달팽이는올라가고싶다.java
+++ src/boj/BOJ_2869_달팽이는올라가고싶다.java
@@ -5,21 +5,20 @@ import java.util.*;
 
 public class BOJ_2869_달팽이는올라가고싶다 {
 	public static void main(String[] args) throws IOException {
+		
+		/* 도저히 사고가 안되어 답을 보았다.
+		 * 전체길이에서 내려가는 것을 하나를 빼고
+		 * (올라가는 것 - 내려가는 것)으로 나눈다 
+		 * */
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int a = Integer.parseInt(st.nextToken());
 		int b = Integer.parseInt(st.nextToken());
 		int v = Integer.parseInt(st.nextToken());
 			
-		int day = 0;
-		int now = 0;
-		while(true) {
-			day++;
-			now += a;
-			if(now>= v) {
-				break;
-			}
-			now -= b;
+		int day = (v-b)/(a-b);
+		if((v-b)%(a-b) != 0) {
+			day += 1;
 		}
 		System.out.println(day);
 	}
